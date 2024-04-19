ff6364f94d8054fe18f79aa28e8ecce0d01200d3
vluke123 <vluke123@hotmail.co.uk>
2024-04-19 10:34:28 +0100

    First completed script, output of temp vs period

---
+++ functions.py
@@ -0,0 +1,9 @@
+"""Module to create functions that will be used within main"""
+
+def convert_temperature(temp):
+    
+
+
+
+# TODO: Add some conversion functions to translate the peiod, temperature, semi axis
+#       and mass into standard metric units (for user comprehension)
--- main.py
+++ main.py
@@ -1,77 +1,66 @@
+"""Python script to load exoplanet XML data into a dataframe for manipulation"""
+
 import xml.etree.ElementTree as ET
 import pandas as pd
 import numpy as np
-import urllib.request
-import gzip
-import io
+import matplotlib.pyplot as plt
 
-url = "https://github.com/OpenExoplanetCatalogue/oec_gzip/raw/master/systems.xml.gz"
-xml_file = ET.parse(gzip.GzipFile(fileobj=io.BytesIO(urllib.request.urlopen(url).read())))
-root = xml_file.getroot()
+URL = "https://github.com/OpenExoplanetCatalogue/oec_gzip/raw/master/systems.xml.gz"
+xml_file = ET.parse("systems.xml")
 
-planet_df = pd.DataFrame(
-    np.array([[planet.findtext("mass"), planet.findtext("radius")] for planet in xml_file.findall('.//planet')]))
+# Initiate empty data list for colelcting a list of lists for creating a pandas DataFrame
+data = []
 
-print(planet_df)
-
-
-'''
-# Step 2: Create an empty pandas DataFrame
-df = pd.DataFrame(columns=['column_name_1', 'column_name_2'])
-
-# Step 3: Loop through the XML elements and extract the data you want to include in the DataFrame. 
-# Append each row of data to the DataFrame
+# For each planet in the xml file, retireve the planet's attributes
 for planet in xml_file.findall(".//planet"):
-    for child in planet:
-        # Extract the data from each element using the .find() method
-        column_1_value = child.find('planet').text
-        column_2_value = child.find('name').text
+    data.append(
+        [
+            planet.findtext("name"),
+            pd.to_numeric(planet.findtext("mass")),
+            pd.to_numeric(planet.findtext("semimajoraxis")),
+            pd.to_numeric(planet.findtext("eccentricity")),
+            pd.to_numeric(planet.findtext("temperature")),
+            pd.to_numeric(planet.findtext("period")),
+        ]
+    )
 
-        # Create a list of values for the row
-        row = [column_1_value, column_2_value]
+# Create the DataFrame from the planet data. Clean the data.
+PLANET_DF = pd.DataFrame(data)
+PLANET_DF = PLANET_DF.rename(
+    columns={
+        0: "name",
+        1: "mass",
+        2: "semimajoraxis",
+        3: "eccentricity",
+        4: "temperature",
+        5: "period",
+    },
+)
+PLANET_DF.convert_dtypes()
+PLANET_DF = PLANET_DF.replace("", np.nan)
+PLANET_DF = PLANET_DF.dropna()  # Drops any rows that have missing values
 
-        # Append the row to the DataFrame using .loc[]
-        df.loc[len(df)] = row
+# Plot the data on graphs using matplotlib
+x = PLANET_DF["period"].values
+y = PLANET_DF["temperature"].values
+fig, ax = plt.subplots()
+ax.scatter(
+    x,
+    y,
+    s=2,
+)
 
-    # Step 4: Print the DataFrame to confirm that it contains the tabulated form of the XML data
-print(df)
-'''
+# Create a trendline for the graph using Least Squares regression (i.e Polynomial.fit())
+p_fitted = np.polynomial.Polynomial.fit(np.log(x), np.log(y), 1)
+# The line above creates a polynomial object which when given an x-value will return a y-value
+# according to the Least Squares regression (y = mx + b)
+trend_x = np.linspace(min(x), max(x), 1000)
+trend_y = list(map(lambda x: np.exp(p_fitted(np.log(x))), (trend_x)))
+print(max(trend_y))
+print(max(x))
 
+ax.plot(trend_x, trend_y, color="red", linestyle="--")
 
-
-'''
-# Output mass and radius of all planets 
-for planet in oec.findall(".//planet"):
-    print("mass: " + str(planet.findtext("mass")), 
-          "radius: " + str(planet.findtext("radius")))
- 
-# Find all circumbinary planets 
-for planet in oec.findall(".//binary/planet"):
-    print("name: " + planet.findtext("name"))
- 
-# Output distance to planetary system (in pc, if known) and number of planets in system
-for system in oec.findall(".//system"):
-    print("distance: " + str(system.findtext("distance")),
-          len(system.findall(".//planet")))
-
-for country in root.findall('country'):
-    rank = country.find('rank').text #.find() finds the first child with a particular tag
-    name = country.get('name') #.get() finds the element's attributes, this case it's 'country' names
-    print(name, rank)
-
-# https://docs.python.org/3/library/xml.etree.elementtree.html#elementtree-xpath
-
-[tag='text'] # This can be used to search for a child named 'tag'
-# whose complete text content equals 'text'
-
-#iterfind(match, namespaces=None)
-#Finds all matching subelements, by tag name or path. Returns an iterable yielding all matching elements in document order. namespaces is an optional mapping from namespace prefix to full name.
-
-for planet in root.iterfind('.//planet'):
-    print(planet.findtext('name'))
-
-or
-
-for planet in xml.xml_object.iter('.//planet'):
-    print(planet.findtext('name'))
-'''
+plt.xscale("log")
+plt.yscale("log")
+plt.show()
